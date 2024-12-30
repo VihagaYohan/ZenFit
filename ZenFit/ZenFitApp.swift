26f2ca3654b5f87e26959c237f742a9cfdaaaf9b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-30 08:59:36 +0530

    Initial Commit

---
+++ ZenFit/ZenFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenFitApp.swift
+//  ZenFit
+//
+//  Created by Yohan on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct ZenFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

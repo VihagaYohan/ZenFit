3a5ed32a86bba66b8055e8986acb6927a6df52e5
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-30 09:23:55 +0530

    Initial commit - Added fonts

--- ZenFit/ContentView.swift
+++ ZenFit/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Font.custom("FunnelDisplay-Medium", size: 20))
         }
         .padding()
     }
